4d30937d18f111ffa63734c9fe0f4f7a9a7f5147
parsa3323 <mehr2.business@gmail.com>
2025-06-09 19:13:15 -0700

    Naaahhhhhh

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
